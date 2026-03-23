e9c1415bdd949a02376e28026b53c52aebad786e
vinay-chindam <vinnuhary@gmail.com>
2026-03-23 13:43:49 +0530

    process

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log("Hello jeeeeee ")
-process.exit(0)
-console.log("Hello jeeeeee 2")
+
+require('dotenv').config()
+
+console.log(`Hi I am ${process.env.NAME}, i am a ${process.env.PROFESSION} and i am gonna start ${process.env.COURSE} `)
 
