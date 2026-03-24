3f0f295cc92d48d8656088bd26da47859fbf1f31
vinay-chindam <vinnuhary@gmail.com>
2026-03-24 12:10:01 +0530

    middlewares

--- index.js
+++ index.js
@@ -1,70 +1,87 @@
+const express = require("express");
+const fs = require("fs");
+let mock_data = require("./mockdata");
 
-const express=require('express')
-let mock_data = require('./mockdata');
+const app = express();
 
-const app=express()
+//middlewares
 
-app.get('/',(req,res)=>{
-    return res.send("welcome to home")
-})
+app.use((req, res, next) => {
+  console.log("logging");
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()} : ${req.method} : ${req.path}\n`,
+    (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      req.user_name = "Vinay_c";
+      next();
+    },
+  );
+});
 
-app.get('/about',(req,res)=>{
-    return res.send("welcome to about page")
-})
+app.use((req, res, next) => {
+  console.log("hello from middleware 2 ", req.user_name);
 
+  next();
+});
+
+app.get("/", (req, res) => {
+  return res.send("welcome to home");
+});
+
+app.get("/about", (req, res) => {
+  return res.send("welcome to about page");
+});
 
 //Routes:
 
 // get Users
 
-app.get('/users',(req,res)=>{
-    return res.json(mock_data)
-})
+app.get("/users", (req, res) => {
+  return res.json(mock_data);
+});
 
-
-
-
-
-app.get('/api/users',(req,res)=>{
-    return res.json(mock_data)
-})
+app.get("/api/users", (req, res) => {
+  return res.json(mock_data);
+});
 
 //get User with given id
 
-
-
-app.get('/api/users/:id',(req,res)=>{
-    const id=Number(req.params.id);
-    console.log(`id is ${id}`)
-    const user=mock_data.find(user=>user.id===id)
-    return res.json(user)
-})
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log(`id is ${id}`);
+  const user = mock_data.find((user) => user.id === id);
+  return res.json(user);
+});
 
 //post request
 
-app.post('/api/users',(req,res)=>{
+app.post("/api/users", (req, res) => {
+  const newUser = {
+    id: mock_data.length + 1,
+    name: "john",
+    email: "john@gmail.com",
+  };
+  mock_data[mock_data.length] = newUser;
+  return res.json(newUser);
+});
 
-    const newUser={id:mock_data.length+1,name:"john",email:"john@gmail.com"}
-    mock_data[mock_data.length]=newUser
-    return res.json(newUser)
-})
+app.patch("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = mock_data.find((user) => user.id === id);
+  user.job_title = "abc@gmail.com";
+  return res.json(user);
+});
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  let l1 = mock_data.length;
+  mock_data = mock_data.filter((user) => user.id !== id);
+  let l2 = mock_data.length;
+  return res.json({ len1: l1, len2: l2 });
+});
 
-app.patch('/api/users/:id',(req,res)=>{
-    const id=Number(req.params.id)
-    const user=mock_data.find(user=>user.id===id)
-    user.job_title="abc@gmail.com"
-    return res.json(user)
-})
-app.delete('/api/users/:id',(req,res)=>{
-    const id=Number(req.params.id)
-    let l1=mock_data.length
-    mock_data=mock_data.filter(user=>user.id!==id)
-    let l2=mock_data.length
-    return res.json({len1:l1,len2:l2})
-   
-})
-
-
-app.listen(8000,()=>{
-    console.log("server started at port 8000")
-}) 
+app.listen(8000, () => {
+  console.log("server started at port 8000");
+});
